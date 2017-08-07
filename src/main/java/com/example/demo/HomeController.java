eb82cfb7d493fd553687225e4dd325806b4197f2
jkowalsk1 <jkowalsk@montgomerycollege.edu>
2017-08-07 19:01:59 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+/**
+ * Created by oracle on 8/7/17.
+ */
+@Controller
+public class HomeController {
+    @GetMapping("/songform")
+    public String loadSongForm(Model model){
+        model.addAttribute("song", new Song());
+        return "songform";
+    }
+
+    @PostMapping("/songform")
+    public String processSongForm(@ModelAttribute Song song){
+        return "confirmsong";
+    }
+}
